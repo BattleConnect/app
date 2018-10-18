c6236cb031b1298daea7c820ac0ec1b6354d8257
Christian Atwater <ccatwater@crimson.ua.edu>
2018-10-18 00:03:06 -0500

    Added Login functionality

--- app/src/main/java/com/cs495/battleelite/battleelite/MainActivity.java
+++ app/src/main/java/com/cs495/battleelite/battleelite/MainActivity.java
@@ -18,8 +18,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        startActivityForResult(new Intent(MainActivity.this, LoginActivity.class), 0);
+//        Intent loginIntent = new Intent(MainActivity.this,  LoginActivity.class);
+//        MainActivity.this.startActivity(loginIntent);
 
-        Intent myIntent = new Intent(MainActivity.this, home_screen.class);
-        MainActivity.this.startActivity(myIntent);
+//        Intent myIntent = new Intent(MainActivity.this, home_screen.class);
+//        MainActivity.this.startActivity(myIntent);
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch(requestCode) {
+            case 0:
+                if(resultCode == 1){
+                    Intent myIntent = new Intent(MainActivity.this, home_screen.class);
+                    MainActivity.this.startActivity(myIntent);
+                }
+                break;
+            case 1:
+                //etc...
+                break;
+        }
+    }
+
+
 }
--- app/src/main/java/com/cs495/battleelite/battleelite/home_screen.java
+++ app/src/main/java/com/cs495/battleelite/battleelite/home_screen.java
@@ -11,6 +11,7 @@ public class home_screen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
