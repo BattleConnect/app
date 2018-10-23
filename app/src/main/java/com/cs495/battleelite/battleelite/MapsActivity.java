b9892958f1d06e4d19776b86117003925c3e16af
hsknutson <hsknutson@crimson.ua.edu>
2018-10-22 21:56:54 -0500

    Changed map type to satellite. Fixed sensor type filter.

--- app/src/main/java/com/cs495/battleelite/battleelite/MapsActivity.java
+++ app/src/main/java/com/cs495/battleelite/battleelite/MapsActivity.java
@@ -63,6 +63,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
         mMap.setOnMarkerClickListener(this);
         boundsBuilder = new LatLngBounds.Builder();
         getSensorData();
