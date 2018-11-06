379cfa7138f7ae85b87232b1e3627ea1a6e9a765
Ben_Tegtmeyer <bjtegtmeyer@crimson.ua.edu>
2018-11-06 10:00:00 -0600

    expandable list for filters...not hooked up yet

--- app/src/main/java/com/cs495/battleelite/battleelite/FilterDialogFragment.java
+++ app/src/main/java/com/cs495/battleelite/battleelite/FilterDialogFragment.java
@@ -9,26 +9,46 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ExpandableListView;
 import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class FilterDialogFragment extends DialogFragment {
     FilterDialogFragmentListener mListener;
+    ArrayList<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        // Use the Builder class for convenient dialog construction
+        ExpListViewAdapterWithCheckbox listAdapter;
+        ExpandableListView expListView;
+
+
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(), AlertDialog.THEME_HOLO_LIGHT);
 
         LayoutInflater inflater = getActivity().getLayoutInflater();
         final View v = inflater.inflate(R.layout.dialog_filter, null);
         builder.setView(v);
         builder.setTitle(R.string.filterButton);
+
+
         final Spinner sensorTypes = (Spinner) v.findViewById(R.id.sensorFilterSpinner);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
                 android.R.layout.simple_spinner_item,
                 getResources().getStringArray(R.array.sensorTypeList));
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         sensorTypes.setAdapter(adapter);
+
+
+        loadListData();
+        expListView = (ExpandableListView) v.findViewById(R.id.expandableFilter);
+        listAdapter = new ExpListViewAdapterWithCheckbox(this.getActivity(), listDataHeader, listDataChild);
+        expListView.setAdapter(listAdapter);
+
+
                 builder.setPositiveButton(R.string.filterPositive, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         mListener.getSelectedSensorTypeFilter(sensorTypes.getSelectedItem().toString());
@@ -58,5 +78,25 @@ public class FilterDialogFragment extends DialogFragment {
         } catch (ClassCastException e){
             throw new ClassCastException(getActivity().toString() + " must implement FilterDialogFragmentListener");
         }
+    }
+
+    public void loadListData(){
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+
+        listDataHeader.add("Sensor Type");
+
+
+        List<String> sensorType = new ArrayList();
+        sensorType.add(getResources().getString(R.string.asset));
+        sensorType.add(getResources().getString(R.string.heartbeat));
+        sensorType.add(getResources().getString(R.string.vibration));
+        sensorType.add(getResources().getString(R.string.moisture));
+        sensorType.add(getResources().getString(R.string.temperature));
+
+
+
+        listDataChild.put(listDataHeader.get(0), sensorType);
+
     }
 }
