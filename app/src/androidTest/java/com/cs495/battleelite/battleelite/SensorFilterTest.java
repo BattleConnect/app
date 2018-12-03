6f9a4de0cba9d2ba62752ea4f777a01892e31f30
Ben_Tegtmeyer <bjtegtmeyer@crimson.ua.edu>
2018-12-02 21:53:45 -0600

    start of test

---
+++ app/src/androidTest/java/com/cs495/battleelite/battleelite/SensorFilterTest.java
@@ -0,0 +1,12 @@
+package com.cs495.battleelite.battleelite;
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+public class SensorFilterTest {
+}
